1c334ba225b0497984baa3118975fc250be9fac7
Kelvin Gonçalves <kelvingoncalvestecinfo@gmail.com>
2019-12-04 08:19:02 -0300

    Adicionando Detalhes do Restaurante

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,9 +1,9 @@
-import {Injectable} from '@angular/core'
-import {Http} from '@angular/http'
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
 import { Restaurant } from "./restaurant/restaurant.model";
 
-import 'rxjs/add/operator/map'
-import 'rxjs/add/operator/catch'
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 import { MEAT_API }from '../app.api';
 import { Observable } from 'rxjs/Observable';
@@ -16,6 +16,12 @@ export class RestaurantsService {
 
     restaurants(): Observable<Restaurant[]>{
         return this.http.get(`${MEAT_API}/restaurants`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
+
+    restaurantById(id: string): Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
     }
