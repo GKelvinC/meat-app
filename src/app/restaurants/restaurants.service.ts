fad7a56a23f6970080aa9c2b584b5b4d384b3117
Kelvin Gonçalves <kelvingoncalvestecinfo@gmail.com>
2019-12-05 08:51:07 -0300

    Implementando o shopping cart(add,remove,clear)

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
+
 import { Restaurant } from "./restaurant/restaurant.model";
+import { MenuItem } from "../restaurant-detail/menu-item/menu-item.model"
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -30,5 +32,12 @@ export class RestaurantsService {
         return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+
     }
 }
