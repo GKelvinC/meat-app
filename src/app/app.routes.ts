b89adf1b87b45723bf117f34f0950af84dad35e1
Kelvin Gonçalves <kelvingoncalvestecinfo@gmail.com>
2019-12-16 08:33:54 -0300

    Separando em modulos Lazy-loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from "@angular/router";
 
 
 import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component";
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component"
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
-import { OrderComponent } from "./order/order.component";
 import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 
 export const ROUTES: Routes = [
@@ -19,7 +17,7 @@ export const ROUTES: Routes = [
             {path:'menu',component: MenuComponent},
             {path:'reviews',component: ReviewsComponent},
         ]},
-    {path: 'about',component:AboutComponent},
+    {path: 'order', loadChildren: './order/order.module#OrderModule' },
     {path: 'order-summary', component: OrderSummaryComponent },
-    {path: 'order', component: OrderComponent }
+    {path: 'about', loadChildren: './about/about.module#AboutModule' }
 ]
